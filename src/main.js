6ee1223378cc253e69c9184da13fe6e745db1458
reymark <125524406+Rylllll@users.noreply.github.com>
2023-08-15 15:42:47 +0800

    Animation

--- src/main.js
+++ src/main.js
@@ -10,43 +10,4 @@ function openPopup() {
     popupContainer.classList.remove("show-popup");
   }
 
-// Array of texts to be displayed in the typing animations
-const textsToType = [
-  
-  "Reymark ",
-  "Boquiron",
-
-];
-
-// Decreased typing delay for faster typing speed (adjust this value for speed control)
-const typingDelay = 50; // Adjust this value for faster or slower typing
-
-// Delay between the text1 animation and text2 animation in milliseconds
-const text2Delay = 500; // Adjust this value as needed
-const text3Delay = 100; // Adjust this value as needed
-
-
-// Function to simulate the typing animation
-function typeText() {
-  const textElements = document.querySelectorAll("#typing-container [id^='text']");
-
-  function typeNextCharacter(index, currentIndex = 0) {
-    if (index >= textsToType.length) return; // End animation when all texts are typed
-
-    if (currentIndex < textsToType[index].length) {
-      textElements[index].textContent += textsToType[index][currentIndex];
-      currentIndex++;
-      setTimeout(() => typeNextCharacter(index, currentIndex), typingDelay);
-    } else {
-      // If text1 animation is completed, start the next text animation after the respective delay
-      setTimeout(() => typeNextCharacter(index + 1), index === 0 ? text2Delay : text3Delay);
-    }
-  }
-
-  typeNextCharacter(0);
-}
-
-// Call the typing animation function after the page has loaded
-window.onload = typeText;
-
 
